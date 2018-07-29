3c8738f49f25adb5e6b94807c3f450cfc22a90c9
yuta-ushijima <register@yuta-u.com>
2018-07-29 13:26:32 +0900

    パスの修正

--- app/lib/google/calendar.rb
+++ app/lib/google/calendar.rb
@@ -7,7 +7,6 @@ class Calendar
   OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'.freeze
   APPLICATION_NAME = ENV['APPLICATION_NAME'].freeze
   CREDENTIALS_PATH = ENV['CREDENTIALS_PATH'].freeze
-  # CREDENTIALS_PATH =""
   TOKEN_PATH = 'token.yaml'.freeze
   AUTHORIZE_CODE = ENV['AUTHORIZE_CODE'].freeze
   SCOPE = Google::Apis::CalendarV3::AUTH_CALENDAR_READONLY
